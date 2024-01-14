91446356c9be4fd63a1064f2874dbb0a71bb617e
Rider Sargent <rider.sargent@gmail.com>
2024-01-14 16:01:14 +0000

    Move fetch repo functionality to model.

--- app/controllers/github_users_controller.rb
+++ app/controllers/github_users_controller.rb
@@ -7,51 +7,19 @@ class GithubUsersController < ApplicationController
 
   def show
     @github_user = GithubUser.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to :github_users, alert: 'Github User not found'
+  end
+
+  def new
+    @github_user = GithubUser.new
   end
 
   def create
-    # TODO: Create the GithubUser, repos, number of stars, and language breakdown
-    # @github_user = GithubUser.create(github_user_params)
+    ghu = GithubUser.create_from_github_username(params[:github_user][:username])
 
-    github_user_name = params[:username]
-
-    url = "/users/#{github_user_name}/repos"
-
-    repos = []
-    page = 1
-
-    loop do
-      response = conn.get(url) do |req|
-        req.params['page'] = page
-        req.params['per_page'] = 100
-      end
-
-      break if response.body.empty?
-
-      page += 1
-      repos += response.body
-    end
-
-    render json: repos
-  end
-
-  private
-
-  def github_user_params
-    params.require(:github_user).permit(:username)
-  end
-
-  def conn
-    options = {
-      url: 'https://api.github.com',
-      headers: { 'X-GitHub-Api-Version' => '2022-11-28' }
-    }
-
-    @conn ||= Faraday.new(options) do |builder|
-      builder.request :authorization, 'Bearer', -> { ApiToken.find_by(name: 'github').token }
-      builder.request :json
-      builder.response :json
-      builder.response :raise_error
-    end
+    redirect_to ghu, notice: 'Github User created successfully.'
+  rescue GithubUser::InvalidGithubUserError
+    redirect_to :new_github_user, alert: 'Unable to create Github User - Please try again.'
   end
 end
--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,2 +1,61 @@
 class GithubUser < ApplicationRecord
+  class InvalidGithubUserError < StandardError; end
+
+  has_many :repos, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true
+
+  class << self
+    def create_from_github_username(username)
+      repos = fetch_repos(username)
+      ghu = create!(username: username)
+
+      repos.each do |repo|
+        ghu.repos.create!(
+          name: repo['name'],
+          stars: repo['stargazers_count'],
+          language: repo['language']
+        )
+      end
+
+      ghu
+    end
+
+    def fetch_repos(username)
+      url = "/users/#{username}/repos"
+
+      repos = []
+      page = 1
+
+      loop do
+        response = conn.get(url) do |req|
+          req.params['page'] = page
+          req.params['per_page'] = 100
+        end
+
+        break if response.body.empty?
+
+        page += 1
+        repos += response.body
+      end
+
+      repos
+    rescue Faraday::ResourceNotFound
+      raise InvalidGithubUserError
+    end
+
+    def conn
+      options = {
+        url: 'https://api.github.com',
+        headers: { 'X-GitHub-Api-Version' => '2022-11-28' }
+      }
+
+      @conn ||= Faraday.new(options) do |builder|
+        builder.request :authorization, 'Bearer', -> { ApiToken.find_by(name: 'github').token }
+        builder.request :json
+        builder.response :json
+        builder.response :raise_error
+      end
+    end
+  end
 end
